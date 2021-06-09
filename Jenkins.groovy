985e0758be5ee2456358525b2c629125516abfd9
Sharuk <sharuk.livee@gmail.com>
2021-06-09 17:28:48 +0530

    build file name changed

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -3,7 +3,7 @@ import hudson.plugins.git.*;
 def scm = new GitSCM("https://github.com/Srk-771/Sample-Application.git")
 scm.branches = [new BranchSpec("master")];
 
-def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkinsfile-build.groovy")
+def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition(scm, "Jenkins-Build.groovy")
 
 def parent = Jenkins.instance
 def job = new org.jenkinsci.plugins.workflow.job.WorkflowJob(parent, "Course-Web-App")
